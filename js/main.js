07ba3e5b9704fbe06d63dbbb6a3e99850f67111a
ahmedskulj00 <66512187+ahmedskulj00@users.noreply.github.com>
2021-03-01 11:27:26 +0100

    Implemented functions to enter name and feeling with local storage.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const time = document.getElementById("time")
 const greeting = document.getElementById("greeting")
-
+const name = document.getElementById("name")
+const feeling = document.getElementById("feeling")
 function currentTime(){
     var dt = new Date();
     var hour = dt.getHours();
@@ -36,6 +37,50 @@ function currentTime(){
       }
   }
 
+  function getName(){
+    if(localStorage.getItem('name') === null){
+      name.textContent = '[Enter name]'
+    }else{
+      name.textContent = localStorage.getItem('name')
+    }
+  }
+
+  function setName(e){
+    if(e.type === 'keypress'){
+       if (e.which == 13 || e.keyCode == 13){
+        localStorage.setItem('name', e.target.innerText)
+        name.blur()
+       }
+    }else{
+      localStorage.setItem('name', e.target.innerText)
+    }
+  }
+
+  function getFeeling(){
+    if(localStorage.getItem('feeling') === null){
+      feeling.textContent = '[Enter feeling]'
+    }else{
+      feeling.textContent = localStorage.getItem('feeling')
+    }
+  }
   
+  function setFeeling(e){
+    if (e.type === 'keypress'){
+      if (e.which == 13 || e.keyCode == 13){
+        localStorage.setItem('feeling', e.target.innerText)
+        feeling.blur()
+      }
+    }else{
+      localStorage.setItem('feeling', e.target.innerText)
+    }
+  }
+
+name.addEventListener("keypress", setName)
+name.addEventListener("blur", setName)
+feeling.addEventListener("keypress", setFeeling)
+feeling.addEventListener("blur", setFeeling)
+
   currentTime();
   updateGreeting();
+  getName();
+  getFeeling();
